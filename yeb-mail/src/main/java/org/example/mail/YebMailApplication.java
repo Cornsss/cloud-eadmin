c17b3508736b48e1f991dac89ff796ad4cde5042
danyang2 <zhuling0619@163.com>
2021-03-26 09:21:56 +0800

    测试git功能-1

---
+++ yeb-mail/src/main/java/org/example/mail/YebMailApplication.java
@@ -0,0 +1,13 @@
+package org.example.mail;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class YebMailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YebMailApplication.class);
+    }
+}
